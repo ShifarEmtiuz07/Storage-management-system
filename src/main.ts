6dd469f17a40ff9acd2d76785b89efc3085fc5aa
ShifarEmtiuz07 <shifar16@cse.pstu.ac.bd>
2025-07-22 23:06:33 +0600

    Alhamdulilah project complete, push main branch and work on cors enable

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
+      app.enableCors({
+      origin: true,
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
